2e11a3563954807e263745d4c442134d43e53091
aasthasharmaresume <aastha.india3@gmail.com>
2025-11-27 16:06:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { MessageWall } from "@/components/messages/message-wall";
 import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
